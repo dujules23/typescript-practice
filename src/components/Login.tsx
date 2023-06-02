d74e63f25b6366260a8800a305c5e3baad368139
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-06-01 21:51:41 -0400

    added onChange and onSubmit with private functions, works as intended

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, SyntheticEvent } from 'react'
 import { AuthService } from '@/services/AuthService'
 
 interface LoginProps {
@@ -12,6 +12,10 @@ interface LoginState {
   loginSuccessful: boolean
 }
 
+interface CustomEvent {
+  target: HTMLInputElement
+}
+
 export class Login extends Component<LoginProps,LoginState> {
   // initial state values
   state: LoginState ={
@@ -21,13 +25,35 @@ export class Login extends Component<LoginProps,LoginState> {
     loginSuccessful: false
   }
 
+  private setUserName(event: CustomEvent){
+    this.setState({userName: event.target.value})
+    // console.log('Setting username to: ' + event.target.value)
+  }
+  private setPassword(event: CustomEvent){
+    this.setState({password: event.target.value})
+  }
+
+  private async handleSubmit(event: SyntheticEvent){
+    event.preventDefault()
+    const result = await this.props.authService.login(
+      this.state.userName,
+      this.state.password
+    )
+
+    if(result) {
+      console.log(result)
+    } else {
+      console.log('wrong login')
+    }
+  } 
+
   render() {
     return (
       <div>
         <h2>Please login</h2>
-        <form action="">
-          <input value={this.state.userName}/><br />
-          <input value={this.state.password} type='password' /><br />
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input value={this.state.userName} onChange={e => this.setUserName(e)}/><br />
+          <input value={this.state.password} type='password' onChange={e => this.setPassword(e)}/><br />
           <input type="submit" value='Login' />
         </form>
       </div>
